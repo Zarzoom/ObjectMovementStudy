fca50bab5a952a60892c9a38f160278bcb0f68ac
Sophia Seely <sophiaseely@outlook.com>
2023-09-29 15:49:33 -0500

    trying to center the card panels.

--- src/Components/Main/Logo.tsx
+++ src/Components/Main/Logo.tsx
@@ -1,11 +1,14 @@
-import {Panel, Col, Row} from "rsuite";
+import {Panel, Col, Row, Whisper, Tooltip} from "rsuite";
 import logo2 from 'src/Images/logo2.png';
 
 export function Logo() {
+    const toolTip= (<Tooltip>Image Description: Logo that says Sophia Seely.</Tooltip>)
 return(
     <Panel bordered={true} className={"logoBackground"}>
         <Col xs={24}>
-            <img src={"logo2"} alt={"Sophia Seely"}/>
+            <Whisper placement="top" controlId="control-id-hover" trigger="hover" speaker={toolTip}>
+                <img src={"logo2"} alt={"Sophia Seely"}/>
+            </Whisper>
         </Col>
     </Panel>
 )
--- src/Components/Main/Portfolio.tsx
+++ src/Components/Main/Portfolio.tsx
@@ -1,20 +1,36 @@
-import {Panel, Col, Placeholder, Button} from "rsuite";
+import {Panel, Stack, Row, Col, Placeholder, Button, Grid} from "rsuite";
 
 export function Portfolio() {
     return(
-        <div>
-        <Panel shaded={true} bordered={true} bodyFill={true} style={{ display: 'inline-block', width: 240 }}>
-            <Placeholder graph={"image"}></Placeholder>
-            <Panel header="Household Inventory System">
-                <p>
-                    <small>
-                        A program for keeping inventory of the contents of your home.
-                    </small>
-                </p>
-            </Panel>
-        </Panel>
+        <Grid fluid>
+        <div className={"horizontalCenter"}>
+        <Stack spacing={3} alignItems={"center"}>
+                <Panel shaded={true} bordered={true} bodyFill={true} style={{ display: 'inline-block', width: 240 }}>
+                    <Placeholder graph={"image"}></Placeholder>
+                    <Panel header="Household Inventory System">
+                        <p>
+                            <small>
+                                A program for keeping inventory of the contents of your home.
+                            </small>
+                        </p>
+                    </Panel>
+                </Panel>
 
-        <Button appearance={"primary"} color={"blue"}>Test1</Button>
+                <Panel shaded={true} bordered={true} bodyFill={true} style={{ display: 'inline-block', width: 240 }}>
+                    <Placeholder graph={"image"}></Placeholder>
+                    <Panel header="Household Inventory System">
+                        <p>
+                            <small>
+                                A program for keeping inventory of the contents of your home.
+                            </small>
+                        </p>
+                    </Panel>
+                </Panel>
+        </Stack>
+            <div className={"fontColor"}>
+            <h1>Testing</h1>
+            </div>
         </div>
+        </Grid>
     )
 }
